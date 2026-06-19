8ce1a4ec97de7bb3c0cc284d46650992272740e2
Shaurya1803 <shauryatiwari157@gmail.com>
2026-06-19 22:49:31 +0530

    idk

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 
-// Use environment variable if available (for production), otherwise use config (for development)
+
 const mongoURI = process.env.MONGODB_URI || config.get("MONGODB_URI");
 
 mongoose.connect(mongoURI)

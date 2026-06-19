1d44ed20f94d5ded1bbd71471252e7d7f41ef6e3
Shaurya1803 <shauryatiwari157@gmail.com>
2026-06-19 22:24:08 +0530

    Security: Remove hardcoded MongoDB credentials from config

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 
-mongoose.connect(config.get("MONGODB_URI"))
+// Use environment variable if available (for production), otherwise use config (for development)
+const mongoURI = process.env.MONGODB_URI || config.get("MONGODB_URI");
+
+mongoose.connect(mongoURI)
 .then(function(){
    dbgr("connected");
 })

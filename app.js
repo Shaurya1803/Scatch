12bd1fb01742250487a9302c5ee49394b06926d7
Shaurya1803 <shauryatiwari157@gmail.com>
2026-06-19 20:14:05 +0530

    idk

--- app.js
+++ app.js
@@ -38,4 +38,5 @@ app.get("/", (req, res) => {
     res.render("index", { error: "", isAdmin: req.session?.isAdmin || false });
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

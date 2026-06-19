abe32728a79693501caa3cf801709571241254b7
Shaurya1803 <shauryatiwari157@gmail.com>
2026-06-19 20:35:01 +0530

    Fix: Correct MongoDB connection string format

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const config = require("config");
 const dbgr = require("debug")("development:mongoose");
 
-mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
+mongoose.connect(config.get("MONGODB_URI"))
 .then(function(){
-   dbgr("connected");$env:NODE_ENV="development"
+   dbgr("connected");
 })
 .catch(function(err){
     dbgr(err);
